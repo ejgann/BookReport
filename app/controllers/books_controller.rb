666481e2aebfe6a677fe803401b9300b81613a17
Emily Gann <aspentree79@gmail.com>
2020-06-30 12:38:20 -0400

    built out books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,33 @@
 class BooksController < ApplicationController
+    before_action :redirect_if_not_logged_in
+   
+    def index
+        @books = Book.all
+    end
+    
+    def new
+        @book = Book.new
+    end
 
+    def create
+        @book = current_user.books.build(book_params)
+        if @book.save
+            redirect_to books_path
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :author)
+    end
 
 end
