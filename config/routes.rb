5b90f9921c384808777273295998a5a5b7ba43d8
Emily Gann <aspentree79@gmail.com>
2020-06-30 11:44:53 -0400

    build out sessions controller and added session view files

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@ Rails.application.routes.draw do
   root 'sessions#home'
   
   # signup
-  get '/signup' to: 'users#new'
+  get '/signup', to: 'users#new'
 
   # login
-  get '/login' to: 'sessions#new'
-  post '/login' to: 'sessions#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 
   # logout
-  delete '/logout' to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
 
   resources :reviews
   resources :books
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
