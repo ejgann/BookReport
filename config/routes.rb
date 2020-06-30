8ce79d5f0ddcbede216ee0bbec44dba439667385
Emily Gann <aspentree79@gmail.com>
2020-06-30 11:33:22 -0400

    established initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   root 'sessions#home'
   
+  # signup
+  get '/signup' to: 'users#new'
+
+  # login
+  get '/login' to: 'sessions#new'
+  post '/login' to: 'sessions#create'
+
+  # logout
+  delete '/logout' to: 'sessions#destroy'
+
+
   resources :reviews
   resources :books
   resources :users
